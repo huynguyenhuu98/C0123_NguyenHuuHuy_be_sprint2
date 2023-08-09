f874615479d5f58ad5ab0ce1bdc1ada9b6145681
huynguyenhuu98 <121575466+huynguyenhuu98@users.noreply.github.com>
2023-08-09 14:00:23 +0700

    update be sprint 2

---
+++ backend_sprint2/src/test/java/com/example/backend_sprint2/BackendSprint2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_sprint2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSprint2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
